d30081ee19c461f8d5a174bb8561ee635a7ba612
Vaibhav-Dewangan <vaibhavdewangan28@gmail.com>
2025-01-20 00:10:20 +0530

    question details ui updated

--- client/question-details.php
+++ client/question-details.php
@@ -1,6 +1,6 @@
-<div class="container mt-5 pb-5 d-flex gap-5">
-    <div class='col-8'>
-        <h2>Question</h2>
+<div class="container mt-5 pb-5 px-4 px-lg-5 px-xl-0 d-flex gap-xl-3 justify-content-between">
+    <div class='col-xl-8 col-md-7'>
+        <h2 class="font-title">Question</h2>
         <?php
         include('./common/db.php');
         $query = "SELECT * FROM `questions` WHERE id='$qid'";
@@ -10,33 +10,33 @@
         $description = $row['description'];
         $category = $row['category'];
         echo "
-     <div class='mt-4'>
+     <div class='mt-4 font-size'>
         <h5 class=' text-primary'>$title</h5>
         <p class='py-2'>$description</p>
     </div>"
         ?>
-        <form action='./server/requests.php' method='post'>
+        <form class="font-size" action='./server/requests.php' method='post'>
             <input type="hidden" name="question_id" value="<?php echo $qid ?>">
             <textarea name='answer' id='answer' placeholder='Your answer' class='form-control w-75 mb-3 '></textarea>
             <button type='submit' name='answer-submit' class='btn btn-primary'>Submit Your Answer</button>
         </form>
 
-        <h2 class="my-4">Answers</h2>
+        <h2 class="my-4 font-title">Answers</h2>
         <?php
         $query = "SELECT * FROM `answers` WHERE question_id='$qid'";
         $result = $conn->query($query);
         foreach ($result as $row) {
             $username = $row['username'];
             $answer = $row['answer'];
-            echo "<div class='bg-light p-3 my-3 rounded-2'>
+            echo "<div class='bg-light p-3 my-3 rounded-2 mr-xl-3'>
                <h6 class=''>$username</h6>
                <p>$answer</p>
              </div>";
         }
         ?>
     </div>
-    <div class="col-4">
-        <h2 class="">Related questions</h2>
+    <div class="col-lg-4 d-none d-md-block">
+        <h2 class="font-title">Related questions</h2>
         <?php
         include('./common/db.php');
         $query = "SELECT * FROM `questions` WHERE category='$category'AND id!='$qid'";
@@ -49,7 +49,7 @@
             echo "
         <a  href='?q-id=$id' class='text-decoration-none'>
          <div class='py-2 px-4  border bg-light rounded my-3 '>
-             <h5 class=' text-black '>$title</h5>
+             <h5 class=' text-black font-size '>$title</h5>
          </div>
         </a>";
         }
